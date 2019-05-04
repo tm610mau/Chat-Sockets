be69b968c19e1a72e7615afb160084776b746b2c
Racso <kevin.racso@gmail.com>
2019-05-04 13:30:55 -0400

    Server y cliente
    sólo acepta naturales como saldo, pero tiene problemas para recibir clientes luego de la primera desconexión

---
+++ cliente.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 22:49:36 2019
+
+@author: Kevin Racso
+"""
+
+############################ Llamar a este archivo cliente.py ################################################
+
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python3
+"""Script for Tkinter GUI chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter
+
+#----Now comes the sockets part----
+#HOST = input('Enter host: ')
+HOST = '127.0.0.1'
+PORT = 33000
+#PORT = input('Enter port: ')
+#if not PORT:
+#    PORT = 33000
+#else:
+#    PORT = int(PORT)
+
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            msg = client_socket.recv(BUFSIZ).decode('UTF-8')
+            if msg == ":q": #terminar la sesion si el cliente recibe un :q del servidor
+                client_socket.close()
+                print("You have left the quantum chat")
+                top.destroy()
+            else:
+                msg_list.insert(tkinter.END, msg)
+        except OSError:  # Possibly client has left the chat.
+            break
+
+
+def send(event=None):  # event is passed by binders.
+    """Handles sending of messages."""
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, 'UTF-8'))
+#    if msg == ":q":
+#        client_socket.close()
+#        top.quit()
+#        print("You have left the quantum chat")
+
+
+def on_closing(event=None):
+    """This function is to be called when the window is closed."""
+    my_msg.set(":q")
+    send()
+
+top = tkinter.Tk()
+top.title("Quantum Chat")
+
+messages_frame = tkinter.Frame(top)
+my_msg = tkinter.StringVar()  # For the messages to be sent.
+#my_msg.set("Type your nickname here")
+my_msg.set("Type your message here")
+scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = tkinter.Listbox(messages_frame, height=20, width=75, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = tkinter.Button(top, text="Talk like a normal human being", command=send)
+send_button.pack()
+
+top.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+tkinter.mainloop() # Starts GUI execution.
+
+#entry_field = tkinter.Entry(top, textvariable=my_msg)
+#entry_field.bind("<Return>", send)
+#entry_field.pack()
+#send_button = tkinter.Button(top, text="Talk like a normal human being", command=send)
+#send_button.pack()
+#
+#top.protocol("WM_DELETE_WINDOW", on_closing)
+#
+#
+#
+#receive_thread = Thread(target=receive)
+#receive_thread.start()
+#tkinter.mainloop() # Starts GUI execution.
---
+++ server.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  1 22:45:10 2019
+
+@author: Kevin Racso
+"""
+
+############################ Llamar a este archivo server.py ################################################
+# -*- coding: utf-8 -*- 
+
+
+#!/usr/bin/env python3
+"""Server for multithreaded (asynchronous) chat application."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread, Lock
+
+
+def welcome():
+    """Dar bienvenida, a menos que se haya superado el limite de clientes."""
+    while True:
+        client, address = SERVER.accept()
+        CONNECTIONS[client] = client
+        if len(CONNECTIONS) > CLIENTLIMIT: # si se llega al limite, no darle la bienvenida
+            client.send(bytes("This chat is full, begone  (type :q)", "utf8"))
+        
+        else:
+            print("<%s:%s> has connected." % address)
+            client.send(bytes("Greetings fellow humans to this totally legit human chat!", "utf8"))
+            ADDRESSES [client] = address
+        
+        Thread(target=enter_chat, args=(client,address,)).start()
+        
+
+def enter_chat(client, address):
+    """Permitir que el cliente interactue en el chat, a menos que se haya superado el limite de clientes."""
+
+    if len(CONNECTIONS) > CLIENTLIMIT: # esperar que cierre su chat, pero no dejarlo interactuar con los demas
+
+            CONNECTIONS.pop(client) #remover cliente de lista de conecciones
+            
+            while True:
+                messages = client.recv(BUFFERSIZE)
+                if messages == bytes(":q", 'UTF-8'):
+                    client.send(bytes(":q", 'UTF-8'))
+                    client.close()
+                    break
+    else:
+        name = client.recv(BUFFERSIZE).decode('UTF-8')
+        while not_unique_name(name): # chequear que el nombre no este ya en la lista
+            client.send(bytes('The name %s is already taken, try again' % name , 'UTF-8'))
+        
+            name = client.recv(BUFFERSIZE).decode('UTF-8')
+            
+        NAMES[client] = name; # si es unico, agregar nombre a la lista 
+        
+        #saludar y preguntar al cliente por su saldo
+        client.send(bytes('Welcome %s! What is your balance?' % name, 'UTF-8'))
+        
+        
+        while True:
+            
+            #saludar y preguntar al cliente por su saldo
+            # client.send(bytes('Welcome %s! What is your balance?' % name, 'UTF-8'))
+            saldo = client.recv(BUFFERSIZE).decode('UTF-8') # K: Definir saldo
+            
+            try: 
+                saldo_num = float(saldo)
+                
+                if saldo_num<0:
+                
+                    client.send(bytes('The balance you entered is not valid. Please enter your balance', 'UTF-8'))
+                
+                elif not saldo_num.is_integer():
+                
+                    client.send(bytes('The balance you entered is not valid. Please enter your balance', 'UTF-8'))
+                
+                else:
+                    break
+            
+            except ValueError:
+                
+                client.send(bytes('The balance you entered is not valid. Please enter your balance', 'UTF-8'))
+    
+
+        #saldo = client.recv(BUFFERSIZE).decode('UTF-8') # K: Definir saldo
+        #print(saldo)
+        BALANCES[client] = int(saldo)
+        client.send(bytes('The balance of %s has been set to your account' % saldo, 'UTF-8'))
+        # informar a todos que llego un nuevo usuario
+        broadcast(bytes("User <%s> has graced this chat with their presence" % name,'UTF-8'),client)
+        
+        
+        while True:
+            messages = client.recv(BUFFERSIZE)
+            msgstring = str(messages, 'UTF-8') 
+            if messages == bytes(":q", 'UTF-8'):
+                
+                client.send(bytes(":q", 'UTF-8'))
+                client.close()
+                broadcast(bytes("%s has disconnected." % name, 'UTF-8'),client)
+                print("<%s:%s> has disconnected." % address)
+                CONNECTIONS.pop(client)
+                ADDRESSES.pop(client)
+                NAMES.pop(client)
+                break
+            
+            elif messages == bytes(":funds", 'UTF-8'):   
+                client.send(bytes("You have %s dollars" % saldo, 'UTF-8'))
+                
+            
+            #elif bytes(":t", 'UTF-8') in messages:
+            elif msgstring.startswith(":t"):  
+                client.send(bytes("you've done it", 'UTF-8'))
+            
+            #elif msgstring.startswith(":p"):
+                #target = ...
+                
+            elif msgstring.startswith(":p - <Identifier> - "):
+                
+                
+                
+            else:
+                client.send(bytes("Me :",'UTF-8') + messages)
+                broadcast(messages, client, "<%s> : " % name)   
+                
+
+
+def broadcast(message, client, prefix=""):  # prefix is for name identification.
+    """Mandar mensaje a todos los clientes"""
+        
+    for i in NAMES:
+        if i != client:
+            i.send(bytes(prefix, 'UTF-8')+message)
+        
+def not_unique_name(name):
+    
+    for i in NAMES:   
+        if NAMES[i] == name: # si nombre esta en la lista, retornar False
+            
+            return True
+        
+    return False # si no esta, retornar True
+
+mutex = Lock()
+
+CLIENTLIMIT = 2 # limite de clientes
+CONNECTIONS = {} # conecciones activas
+#CLIENTS = {} #clientes
+ADDRESSES = {} # dirreciones de losclientes
+NAMES = {} #nombres de los clientes
+BALANCES = {}
+
+HOST = '127.0.0.1' #asumir que solo se conectara el mismo computador con si mismo
+#HOST = ''
+PORT = 33000
+BUFFERSIZE = 1024 #numero de B maximos
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(6)
+    print("Waiting connection...")
+    ACCEPT_THREAD = Thread(target=welcome)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+SERVER.close()
