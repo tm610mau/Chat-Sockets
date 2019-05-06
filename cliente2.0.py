6222be9c7a07c37a24d2b149703eaa9b4ac61d03
tm610mau <50274872+tm610mau@users.noreply.github.com>
2019-05-06 08:41:39 -0400

    Update cliente2.0.py

--- cliente2.0.py
+++ cliente2.0.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun May  5 01:31:46 2019
-
-@author: Kevin Racso
-"""
-
 ############################ Llamar a este archivo cliente.py ################################################
 
 # -*- coding: utf-8 -*-
@@ -31,69 +24,63 @@ ADDR = (HOST, PORT)
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(ADDR)
 
-def receive():
-    """Handles receiving of messages."""
+def receive_message():
+    """Recibir mensajes"""
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode('UTF-8')
-            if msg == ":q": #terminar la sesion si el cliente recibe un :q del servidor
+            message = client_socket.recv(BUFSIZ).decode('UTF-8')
+            if message == ":q": #terminar la sesion si el cliente recibe un :q del servidor
                 client_socket.close()
-                #print("You have left the quantum chat")
                 top.quit()
                 print("You have left the quantum chat")
                 #top.destroy()
             else:
-                msg_list.insert(tkinter.END, msg)
+                message_history.insert(tkinter.END, message)
         except OSError:  # Possibly client has left the chat.
             break
 
 
 def send(event=None):  # event is passed by binders.
-    """Handles sending of messages."""
-    msg = my_msg.get()
-    my_msg.set("")  # Clears input field.
-    client_socket.send(bytes(msg, 'UTF-8'))
-#    if msg == ":q":
-#        client_socket.close()
-#        top.quit()
-#        print("You have left the quantum chat")
+    """Enviar mensaje al server"""
+    message = client_message.get()
+    client_message.set("")  # Clears input field.
+    client_socket.send(bytes(message, 'UTF-8'))
 
 
-def on_closing(event=None):
-    """This function is to be called when the window is closed."""
-    my_msg.set(":q")
-    send()
+def quit_chat(event=None):
+    """Mandar mensaje de termino de sesion cuando se cierra la ventana"""
+    client_message.set(":q")
+    send() #al enviar :q, el server envia un :q de vuelta que cierra la sesion por completo
 
 top = tkinter.Tk()
 top.title("Quantum Chat")
 
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-#my_msg.set("Type your nickname here")
-my_msg.set("Type your message here")
+client_message = tkinter.StringVar()  # For the messages to be sent.
+client_message.set("Type your Username here")
 scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
 xbar = tkinter.Scrollbar(messages_frame, orient='horizontal')
 #xbar = tkinter.Scrollbar(messages_frame) # scrollbar horizontal
 # Following will contain the messages.
-msg_list = tkinter.Listbox(messages_frame, height=20, width=80, yscrollcommand=scrollbar.set, xscrollcommand=xbar.set)
+message_history = tkinter.Listbox(messages_frame, height=20, width=80, yscrollcommand=scrollbar.set, xscrollcommand=xbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 xbar.pack(side=tkinter.BOTTOM, fill=tkinter.X)
-xbar.config(command=msg_list.xview)
-scrollbar.config(command=msg_list.yview)
-msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
+xbar.config(command=message_history.xview)
+scrollbar.config(command=message_history.yview)
+message_history.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
+message_history.pack()
 messages_frame.pack()
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack()
-send_button = tkinter.Button(top, text="Talk like a normal human being", command=send)
-send_button.pack()
+entry = tkinter.Entry(top, textvariable=client_message)
+entry.bind("<Return>", send)
+entry.pack()
+button = tkinter.Button(top, text="Talk like a normal human being", command=send)
+button.pack()
 
-top.protocol("WM_DELETE_WINDOW", on_closing)
+top.protocol("WM_DELETE_WINDOW", quit_chat)
 
 
 
-receive_thread = Thread(target=receive)
+receive_thread = Thread(target=receive_message)
 receive_thread.start()
 tkinter.mainloop() # Starts GUI execution.
